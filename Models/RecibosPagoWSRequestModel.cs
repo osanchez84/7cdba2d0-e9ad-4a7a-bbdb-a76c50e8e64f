f3ed1c206f18e5f9cfa4e0f8127fe06109b03e73
Rubén Emmanuel Alquicira Arrieta <ruben.alquicira@interfactura.com>
2023-08-06 19:11:38 -0600

    Web app V4

---
+++ Models/RecibosPagoWSRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GuanajuatoAdminUsuarios.Models
+{
+    public class RecibosPagoWSRequestModel
+    {
+        public string UsuarioLog { get; set; }
+        public string PasswordLog { get; set; }
+        public int ReciboControlInterno { get; set; }
+        [DisplayFormat(DataFormatString = "{yyyy-MM-dd}")]
+        public DateTime FechaReversa { get; set; }
+    }
+}

89c1badc452c649ca7102cbc946e67a9a2917e46
JuanM29 <juan.martinez@inssoftmx.com>
2023-10-23 09:47:43 -0600

    Incidecias 20 Octubre e inicio mudulo busqueda especial

---
+++ Interfaces/IBusquedaEspecialAccidentesService.cs
@@ -0,0 +1,13 @@
+﻿using GuanajuatoAdminUsuarios.Models;
+using System.Collections.Generic;
+
+namespace GuanajuatoAdminUsuarios.Interfaces
+{
+    public interface IBusquedaEspecialAccidentesService
+    {
+        List<BusquedaEspecialAccidentesModel> BusquedaAccidentes(BusquedaEspecialAccidentesModel model, int idOficina);
+        List<BusquedaAccidentesPDFModel> BusquedaAccidentes(BusquedaAccidentesPDFModel model, int idOficina);
+        public BusquedaAccidentesPDFModel ObtenerAccidentePorId(int idAccidente);
+        List<BusquedaEspecialAccidentesModel> ObtenerAccidentes(BusquedaEspecialAccidentesModel model);
+    }
+}

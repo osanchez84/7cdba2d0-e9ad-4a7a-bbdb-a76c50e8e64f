fa0f8f9d79f32f00700632f3191b276928c2c1be
JuanM29 <134429003+JuanM29@users.noreply.github.com>
2023-11-27 13:09:54 -0600

    Ajustes busqueda personas (#163)
    * Ajuste para mostrar datoscompletos de conductor
    
    * Ajustes a consulta paraevitar duplicados y cambios para llenado correcto de informacion
    
    * Ajustes para atender incidencias varias
    
    * atencion a incidencias 16 octubre
    
    * Incidencias 17 Octubre
    
    * Inciencias 18 Octubre
    
    * Incidencias 19 Octubre y Columnas faltantes busqueda Accidentes
    
    * Incidecias 20 Octubre e inicio mudulo busqueda especial
    
    * Consumo de servicio login y se agrega modulo busqueda especial al menu
    
    * moduloingrreso vehiculo y flujo corregido
    
    * Modulo ingreso de vehiculo e incidencias
    
    * Modulo salida de vehiculo 90%
    
    * Ultimos cambios modulo salida vehiculo 90%
    
    * Campos faltantes en accidentes
    
    * Modulo busqueda deposito completo y atencion de incidencias
    
    * correccion de error en modulo cancelar infraccion
    
    * Atencio a comentarios documento comparativo
    
    * Resolucion e conflictos
    
    * Correccion de errores en el merge
    
    * Atencio a comentarios estadisticas accidentes e infracciones
    
    * Ajustes en login y atencion de comentarios e incidencias
    
    * Tabla y excel estadisticas infracciones y carga de info busqueda accidentes
    
    * Se atienden comentarios comparativos seccion accidentes
    
    * solucion duplicado en merge
    
    * atencion a comentarios padron gruas,asignasion gruas y liberacion vehiculos
    
    * atencion incidencias prioritarias
    
    * cMerge branch 'main' of https://github.com/Inssoft-proyects/7cdba2d0-e9ad-4a7a-bbdb-a76c50e8e64f into moduloSalidaVehiculos
    
    * Ajustes de incidencias en factores de accidente
    
    * correccion incidencia telefono no obligatorio personas morales
    
    * incidencia con datos de vehiculos
    
    * se atiende incidencia en causas
    
    * Se agregan campos de busuqeda a los data table
    
    * se agrega searchbar a tabla faltante y se filtra delegacion por oficina usuario
    
    * Ajustes para filtrar carreteras por oficina delegacion del usuario
    
    * correccion de errores en merge
    
    * ajsute a crear infraccion desde accidentes y correccion de dropdowns en mosulo accidentes
    
    * se resulve error al mostar modal
    
    * Ultimos ajustes a infraccion desde accidentes
    
    * Correccion filtro tipo motivo, accidentescon decimales y modificacion busqueda personas

--- Controllers/CapturaAccidentesController.cs
+++ Controllers/CapturaAccidentesController.cs
@@ -1269,9 +1269,9 @@ namespace GuanajuatoAdminUsuarios.Controllers
 		{
 			List<CapturaAccidentesModel> ListaInvolucrados = new List<CapturaAccidentesModel>();
 			string parametros = "";
-			parametros += string.IsNullOrEmpty(model.licencia) ? "" : "licencia=" + model.licencia;
-			parametros += string.IsNullOrEmpty(model.curp) ? "" : "curp=" + model.curp + "&";
-			parametros += string.IsNullOrEmpty(model.rfc) ? "" : "rfc=" + model.rfc + "&";
+			parametros += string.IsNullOrEmpty(model.numeroLicencia) ? "" : "licencia=" + model.numeroLicencia;
+			parametros += string.IsNullOrEmpty(model.CURP) ? "" : "curp=" + model.CURP + "&";
+			parametros += string.IsNullOrEmpty(model.RFC) ? "" : "rfc=" + model.RFC + "&";
 			parametros += string.IsNullOrEmpty(model.nombre) ? "" : "nombre=" + model.nombre + "&";
 			parametros += string.IsNullOrEmpty(model.apellidoPaterno) ? "" : "primer_apellido=" + model.apellidoPaterno + "&";
 			parametros += string.IsNullOrEmpty(model.apellidoMaterno) ? "" : "segundo_apellido=" + model.apellidoMaterno + "";
@@ -1284,9 +1284,9 @@ namespace GuanajuatoAdminUsuarios.Controllers
 			// realiza la búsqueda de personas y devuelve los resultados en formato JSON
 
 			PersonaModel persona = new PersonaModel();
-			persona.numeroLicenciaBusqueda = model.licencia;
-			persona.CURPBusqueda = model.curp;
-			persona.RFCBusqueda = model.rfc;
+			persona.numeroLicenciaBusqueda = model.numeroLicencia;
+			persona.CURPBusqueda = model.CURP;
+			persona.RFCBusqueda = model.RFC;
 			persona.nombreBusqueda = model.nombre;
 			persona.apellidoPaternoBusqueda = model.apellidoPaterno;
 			persona.apellidoMaternoBusqueda = model.apellidoMaterno;
@@ -1300,9 +1300,9 @@ namespace GuanajuatoAdminUsuarios.Controllers
 					involucrado.nombre = p.nombre;
 					involucrado.apellidoPaterno = p.apellidoPaterno;
 					involucrado.apellidoMaterno = p.apellidoMaterno;
-					involucrado.rfc = p.RFC;
-					involucrado.curp = p.CURP;
-					involucrado.licencia = p.numeroLicencia;
+					involucrado.RFC = p.RFC;
+					involucrado.CURP = p.CURP;
+					involucrado.numeroLicencia = p.numeroLicencia;
 					involucrado.fechaNacimiento = p.fechaNacimiento.Value;
 					ListaInvolucrados.Add(involucrado);
 				}
@@ -1354,9 +1354,9 @@ namespace GuanajuatoAdminUsuarios.Controllers
                 involucrado.nombre = datosTabla.nombre;
                 involucrado.apellidoPaterno = datosTabla.apellidoPaterno;
                 involucrado.apellidoMaterno = datosTabla.apellidoMaterno;
-                involucrado.rfc = datosTabla.RFC;
-                involucrado.curp = datosTabla.CURP;
-                involucrado.licencia = datosTabla.numeroLicencia; 
+                involucrado.RFC = datosTabla.RFC;
+                involucrado.CURP = datosTabla.CURP;
+                involucrado.numeroLicencia = datosTabla.numeroLicencia; 
                 
                 return Json(involucrado);
             }

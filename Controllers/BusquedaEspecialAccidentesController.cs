972bd074a71e9dfa0cb710f69d20a057fddd4ee8
cristopher <eduardo.orozco@inssoftmx.com>
2024-01-05 13:25:38 -0600

    ajustes

--- Controllers/BusquedaEspecialAccidentesController.cs
+++ Controllers/BusquedaEspecialAccidentesController.cs
@@ -241,5 +241,19 @@ namespace GuanajuatoAdminUsuarios.Controllers
 
         }
 
+
+
+
+        public ActionResult UpdateFolio(string id,string folio)
+        {
+
+
+            var fol = _busquedaEspecialAccidentesService.UpdateFolio(id,folio);
+
+            return Json(true);
+        }
+
+
+
     }
 }

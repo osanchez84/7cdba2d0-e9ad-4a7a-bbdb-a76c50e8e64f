be2d1b8e4c3b3326d582033f40c78ee60331d93c
Diana-Bocardo <148112901+Diana-Bocardo@users.noreply.github.com>
2024-01-12 11:13:50 -0600

    Ajustes Actualización Contraseña

--- Controllers/ConfiguracionesController.cs
+++ Controllers/ConfiguracionesController.cs
@@ -45,72 +45,8 @@ namespace GuanajuatoAdminUsuarios.Controllers
 
 					HttpResponseMessage response = await client.GetAsync(url);
 
-					if (response.IsSuccessStatusCode)
-					{
-						string content = await response.Content.ReadAsStringAsync();
-						dynamic json = JsonConvert.DeserializeObject(content);
 
-						if (json != null && json.Count > 0)
-						{
-							string nombre = json[0].nombre;
-							string oficina = json[0].oficina;
-							string idDependenciaStr = json[0].tipo_oficina;
-
-							if (int.TryParse(idDependenciaStr, out int idDependencia))
-							{
-								HttpContext.Session.SetInt32("IdDependencia", idDependencia);
-
-							}
-							else
-							{
-
-							}
-							string idOficinaStr = json[0].clave_oficina;
-							// string idDependenciaStr = json[0].tipo_oficina;
-							string idUsuario = json[0].idUsuario;
-							string TipoOfi = json[0].tipo_oficina;
-
-							if (int.TryParse(idOficinaStr, out int idOficina))
-							{
-								HttpContext.Session.SetInt32("IdOficina", idOficina);
-
-							}
-							else
-							{
-
-							}
-
-
-							string delegacion = Regex.Match(oficina, @"\|(.+)").Groups[1].Value.Trim();
-
-							List<RespuestaServicio> listaRespuestas = JsonConvert.DeserializeObject<List<RespuestaServicio>>(content);
-							string vectorString = listaRespuestas.FirstOrDefault()?.Vector;
-							if (!string.IsNullOrEmpty(vectorString))
-							{
-								List<int> listaIdsPermitidos = vectorString.Split(',').Select(int.Parse).ToList();
-								string listaIdsPermitidosJson = JsonConvert.SerializeObject(listaIdsPermitidos);
-
-								// Guardar la lista en la variable de sesión
-								HttpContext.Session.SetString("IdsPermitidos", listaIdsPermitidosJson);
-								HttpContext.Session.SetString("Nombre", nombre);
-								HttpContext.Session.SetString("Oficina", oficina);
-								// HttpContext.Session.SetInt32("IdDependencia", idDependencia);
-
-								return Json(listaIdsPermitidosJson);
-							}
-						}
-					}
-
-
-
-					// En caso de respuestas inválidas o vacías, limpiar la variable de sesión
-					HttpContext.Session.Remove("IdsPermitidos");
-					HttpContext.Session.Remove("Nombre");
-					HttpContext.Session.Remove("Oficina");
-					HttpContext.Session.Remove("IdDependencia");
-
-
-					return BadRequest("Error en la respuesta del servicio");
+					return Json(url);
 				}
 			}
 			catch (Exception ex)

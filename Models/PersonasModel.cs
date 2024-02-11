8d5f19897246cd316b0d69012521a1d98afb0683
HMORALESG <152929012+HMORALESG@users.noreply.github.com>
2024-02-11 12:56:35 -0600

    Bugs hmg 2612 (#353)

---
+++ Models/PersonasModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace GuanajuatoAdminUsuarios.Models
+{
+    public class PersonasModel
+    {
+        public PersonaModel PersonaModel { get; set; }
+        public List<PersonaModel> ListadoPersonas { get; set; }
+    }
+}

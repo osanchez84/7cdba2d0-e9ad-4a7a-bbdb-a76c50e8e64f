e1730265647a2ee3a7e68cee4c8babc7c12e0b5d
HMORALESG <humberto.morales@inssoftmx.com>
2024-02-29 13:28:56 -0600

    455, 457 y 464
    455, 457 y 464

--- Services/AsignacionGruasService.cs
+++ Services/AsignacionGruasService.cs
@@ -35,7 +35,7 @@ namespace GuanajuatoAdminUsuarios.Services
                 {
                     connection.Open();
                     SqlCommand command = new SqlCommand("SELECT sol.idSolicitud,sol.folio,sol.fechaSolicitud,sol.idMunicipioUbicacion,sol.idCarreteraUbicacion, " + "" +
-                        "sol.idEntidadUbicacion,sol.vehiculoCalle,sol.vehiculoCarretera,sol.vehiculoColonia,sol.idOficial,sol.idTipoUsuario,sol.idPension,sol.idPropietarioGrua, " +
+                        "sol.idEntidadUbicacion,sol.vehiculoCalle,sol.vehiculoCarretera,sol.vehiculoColonia,sol.idOficial,sol.idTipoUsuario,sol.idPension, ISNULL(X.IdConcesionario,0) idPropietarioGrua, " +
                         "mun.municipio, " +
                         "car.carretera, " +
                         "ent.nombreEntidad, " +
@@ -47,6 +47,7 @@ namespace GuanajuatoAdminUsuarios.Services
                         "LEFT JOIN catEntidades AS ent ON sol.idEntidadUbicacion = ent.idEntidad " +
                         "LEFT JOIN catTiposUsuario AS tip_us ON sol.idTipoUsuario = tip_us.idTipoUsuario " +
                         "LEFT JOIN catOficiales AS ofi ON sol.idOficial = ofi.idOficial " +
+                        "LEFT JOIN depositos x on sol.idSolicitud = X.idSolicitud " +
                         "WHERE sol.folio = @folioBusqueda OR sol.fechaSolicitud = ISNULL(@fechaSolicitud,sol.fechaSolicitud) AND sol.estatus = 1", connection);
 
 

4dbbd72f2ca684ccb3289990b4445a3fac11a014
HMORALESG <humberto.morales@inssoftmx.com>
2024-03-21 14:45:04 -0600

    QI 51

--- Utils/IPdfGenerator.cs
+++ Utils/IPdfGenerator.cs
@@ -1,4 +1,5 @@
-﻿using iTextSharp.text;
+﻿using GuanajuatoAdminUsuarios.Models;
+using iTextSharp.text;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -7,6 +8,7 @@ namespace GuanajuatoAdminUsuarios.Utils
 {
     public interface IPdfGenerator
     {
+        (MemoryStream, string) CreatePdfReporteAsignacion(string NamePdf, string Title, Dictionary<string, string> ColumnsNames, List<ReporteAsignacionModel> ModelData, float[] columnWidth);
         (MemoryStream, string) CreatePdf<T>(string NamePdf, string Title, Dictionary<string, string> ColumnsNames, List<T> ModelData,float[] columnWidth);
         (MemoryStream, string) CreatePdf<T>(string NamePdf, string Title, Dictionary<string, string> ColumnsNames, T ModelData,float[] columnWidth);
         byte[] CreatePDFByHTML(string html, string cssText, Rectangle pageSize);
